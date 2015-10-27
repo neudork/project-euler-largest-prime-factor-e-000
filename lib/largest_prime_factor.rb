a97d4d10d81eabe1a60c25f352d9ab0cece838c2
neudork <dylan596nicholas@gmail.com>
2015-10-27 13:34:09 +0000

    nitrous-backup at Tue Oct 27 13:34:09 UTC 2015

--- lib/largest_prime_factor.rb
+++ lib/largest_prime_factor.rb
@@ -1,1 +1,13 @@
 # Enter your procedural solution here!
+def largest_prime_factor(input)
+  factor = 1
+  factors = Array.new
+  while factor <= input 
+    if input % factor == 0
+      factors.push(factor)
+    end
+    factor =+ 1
+  end
+  factors.max
+end
+
